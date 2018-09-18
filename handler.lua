eed8f94ff0b8a210fb0726e72472635fe457ddbd
Patrick Porto <patrick.porto@corp.globo.com>
2018-09-18 15:30:27 -0300

    Update handler

--- handler.lua
+++ handler.lua
@@ -1,4 +1,7 @@
 local BasePlugin = require "kong.plugins.base_plugin"
+local access = require 'kong.plugins.globo-cache.access'
+local body_filter = require 'kong.plugins.globo-cache.body_filter'
+
 local ProxyCaching = BasePlugin:extend()
 
 ProxyCaching.PRIORITY = 1005
@@ -14,7 +17,12 @@ end
 
 function ProxyCaching:access(config)
     ProxyCaching.super.access(self)
+    access.execute(config)
+end
 
+function ProxyCaching:body_filter(config)
+    ProxyCaching.super.body_filter(self)
+    body_filter.execute(config)
 end
 
 return ProxyCaching
