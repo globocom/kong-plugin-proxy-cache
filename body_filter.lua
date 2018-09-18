b1bf032375801bd3675bb61e8730b6e8c9e2f04b
Patrick Porto <patrick.porto@corp.globo.com>
2018-09-18 15:30:15 -0300

    Add body_filter

---
+++ body_filter.lua
@@ -0,0 +1,28 @@
+local storage = require 'kong.plugins.globo-cache.storage'
+local validators = require 'kong.plugins.globo-cache.validators'
+
+local _M = {}
+
+function _M.execute(config)
+    local chunk, eof = ngx.arg[1], ngx.arg[2]
+
+    storage:new(config)
+    storage:set_config(config)
+
+    if eof then
+        local body = table.concat(ngx.ctx.rt_body_chunks)
+        local cache_key = ngx.var.request_uri
+        ngx.arg[1] = body
+        if validators.check_response_code(config.response_code, ngx.status) and
+           validators.check_request_method(config.request_method) then
+            ngx.log(ngx.DEBUG, "updating cache: ", cache_key)
+            storage:set(cache_key, body)
+        end
+    else
+        ngx.ctx.rt_body_chunks[ngx.ctx.rt_body_chunk_number] = chunk
+        ngx.ctx.rt_body_chunk_number = ngx.ctx.rt_body_chunk_number + 1
+        ngx.arg[1] = nil
+    end
+end
+
+return _M
