147fbc9fd842e60293c827b10c47b9cf8b30607e
Patrick Porto <patrick.porto@corp.globo.com>
2018-09-18 15:29:42 -0300

    Add validators

---
+++ validators.lua
@@ -0,0 +1,25 @@
+local _M = {}
+
+function _M.check_response_code(tab, val)
+    for index, value in ipairs(tab) do
+        if tonumber(value) == val then
+            ngx.log(ngx.DEBUG, 'response code is caching: ', val)
+            return true
+        end
+    end
+    ngx.log(ngx.DEBUG, 'response code is not caching')
+    return false
+end
+
+function _M.check_request_method(request_method)
+    for _, value in ipairs(request_method) do
+        if value == ngx.req.get_method() then
+            ngx.log(ngx.DEBUG, 'request method is caching: ', value)
+            return true
+        end
+    end
+    ngx.log(ngx.DEBUG, 'request method is not caching')
+    return false
+end
+
+return _M
