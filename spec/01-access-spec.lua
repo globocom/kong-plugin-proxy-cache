da20d20a129bec58d2dee2000d0685eea4a3662f
Patrick Porto <patrick.porto@corp.globo.com>
2018-10-02 18:23:36 -0300

    Add tests for Cache-Control

--- spec/01-access-spec.lua
+++ spec/01-access-spec.lua
@@ -11,30 +11,41 @@ for _, strategy in helpers.each_strategy() do
   describe("Proxy Cache: (access) [#" .. strategy .. "]", function()
     local proxy_client
     local red = redis:new()
-    local route1
-    local bp
 
     setup(function()
-      bp = helpers.get_db_utils(strategy)
+        local bp = helpers.get_db_utils(strategy)
+        local route1 = bp.routes:insert({
+            hosts = { "test1.com" },
+        })
+        local route2 = bp.routes:insert({
+            hosts = { "test2.com" },
+        })
+        bp.plugins:insert {
+            name = "proxy-cache",
+            route_id = route1.id,
+            config = {
+                redis = {
+                    host = "localhost"
+                }
+            },
+        }
+        bp.plugins:insert {
+            name = "proxy-cache",
+            route_id = route2.id,
+            config = {
+                cache_control = true,
+                redis = {
+                    host = "localhost"
+                }
+            },
+        }
 
-      route1 = bp.routes:insert({
-        hosts = { "test1.com" },
-      })
-      bp.plugins:insert {
-        name = "proxy-cache",
-        route_id = route1.id,
-        config = {
-          redis = {
-            host = "localhost"
-          }
-        },
-      }
 
-      assert(helpers.start_kong({
-        database   = strategy,
-        nginx_conf = "spec/fixtures/custom_nginx.template",
-        plugins = "bundled,proxy-cache",
-      }))
+        assert(helpers.start_kong({
+            database   = strategy,
+            nginx_conf = "spec/fixtures/custom_nginx.template",
+            plugins = "bundled,proxy-cache",
+        }))
     end)
 
     teardown(function()
@@ -132,30 +143,63 @@ for _, strategy in helpers.each_strategy() do
         assert(cache_status == 'BYPASS', "'X-Cache-Status' must be 'BYPASS'")
       end)
 
-      -- describe("Cache-Control", function()
-      --   after_each(function()
-      --     bp.plugins:insert {
-      --       name = "proxy-cache",
-      --       route_id = route1.id,
-      --       config = {
-      --         redis = {
-      --           host = "localhost"
-      --         },
-      --         cache_control = true
-      --       },
-      --     }
-      --   end)
-      --   it("should contains 'BYPASS' in 'X-Cache-Status' when 'Cache-Control'", function()
-      --     local response = proxy_client:get("/", {
-      --       headers = {
-      --         host = "test1.com",
-      --         cache_control = "no-cache"
-      --       }
-      --     })
-      --     local cache_status = assert.response(response).has.header("X-Cache-Status")
-      --     assert(cache_status == 'BYPASS', "'X-Cache-Status' must be 'BYPASS'")
-      --   end)
-      -- end)
+      describe("when request has Cache-Control", function()
+        it("should contains 'REFRESH' in 'X-Cache-Status' when 'Cache-Control' is 'no-cache'", function()
+            local response = proxy_client:get("/", {
+                headers = {
+                    host = "test2.com",
+                    ['Cache-Control'] = "no-cache"
+                }
+            })
+            local cache_status = assert.response(response).has.header("X-Cache-Status")
+            assert(cache_status == 'REFRESH', "'X-Cache-Status' must be 'REFRESH'")
+        end)
+
+        it("should contains 'MISS' in 'X-Cache-Status' when 'Cache-Control' not found", function()
+            local response = proxy_client:get("/", {
+                headers = {
+                    host = "test2.com",
+                }
+            })
+            local cache_status = assert.response(response).has.header("X-Cache-Status")
+            assert(cache_status == 'MISS', "'X-Cache-Status' must be 'MISS'")
+        end)
+
+        it("should contains 'HIT' in 'X-Cache-Status' when 'Cache-Control' not found and access two times", function()
+            local response = proxy_client:get("/", {
+                headers = {
+                    host = "test2.com",
+                }
+            })
+            local proxy_client2 = helpers.proxy_client()
+            sleep(0.5)
+            local response = proxy_client2:get("/", {
+                headers = {
+                    host = "test2.com"
+                }
+            })
+            local cache_status = assert.response(response).has.header("X-Cache-Status")
+            assert(cache_status == 'HIT', "'X-Cache-Status' must be 'HIT'")
+        end)
+
+        it("should contains 'MISS' in 'X-Cache-Status' when 'max-age' expires", function()
+            local response = proxy_client:get("/", {
+                headers = {
+                    host = "test2.com",
+                    ['Cache-Control'] = "max-age=2"
+                }
+            })
+            local proxy_client2 = helpers.proxy_client()
+            sleep(3)
+            local response = proxy_client2:get("/", {
+                headers = {
+                    host = "test2.com"
+                }
+            })
+            local cache_status = assert.response(response).has.header("X-Cache-Status")
+            assert(cache_status == 'MISS', "'X-Cache-Status' must be 'MISS'")
+        end)
+      end)
     end)
   end)
 end
