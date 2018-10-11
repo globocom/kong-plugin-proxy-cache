dd10c2f5b52e1bdb596526418f8874e37e5036ae
Patrick Porto <patrick.porto@corp.globo.com>
2018-10-11 18:54:09 -0300

    Change miss validation to use redis ttl

--- proxy-cache/access.lua
+++ proxy-cache/access.lua
@@ -36,17 +36,18 @@ function _M.execute(config)
     end
     
     ngx.ctx.cache_key = cache:generate_cache_key(ngx.req, ngx.var)
-    if storage:ttl(ngx.ctx.cache_key) == -1 then
+    local cached_value_ttl = storage:ttl(ngx.ctx.cache_key)
+    if cached_value_ttl == -1 then
         ngx.log(ngx.NOTICE, "[cache-check] the cache key exists but it was expires")
         ngx.header['X-Cache-Status'] = 'EXPIRED'
         return
     end
-    local cached_value, err = storage:get(ngx.ctx.cache_key)
-    if not (cached_value and cached_value ~= ngx.null) then
+    if cached_value_ttl == -2 then
         ngx.log(ngx.NOTICE, "[cache-check] the cache key '"..ngx.ctx.cache_key.."' was not found")
         ngx.header['X-Cache-Status'] = 'MISS'
         return
     end
+    local cached_value, err = storage:get(ngx.ctx.cache_key)
     return render_from_cache(ngx.ctx.cache_key, cached_value)
 end
 
