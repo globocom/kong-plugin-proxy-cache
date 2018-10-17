63b096530c13f51d0578e0425df80425ba44db8a
Patrick Porto <patrick.porto@corp.globo.com>
2018-10-17 18:26:56 -0300

    Refactoring validation in body_filter

--- proxy-cache/body_filter.lua
+++ proxy-cache/body_filter.lua
@@ -5,6 +5,12 @@ local Cache = require 'kong.plugins.proxy-cache.cache'
 local _M = {}
 
 function _M.execute(config)
+    if not (validators.check_response_code(config.response_code, ngx.status) and
+       validators.check_request_method()) then
+        ngx.log(ngx.NOTICE, "[cache-update] the cache was not updated because of plugin config")
+        return
+    end
+
     local chunk, eof = ngx.arg[1], ngx.arg[2]
 
     local storage = Storage:new()
@@ -17,22 +23,16 @@ function _M.execute(config)
         ngx.log(ngx.NOTICE, "[cache-update] response content finished")
         local body = table.concat(ngx.ctx.rt_body_chunks)
         ngx.arg[1] = body
-        if validators.check_response_code(config.response_code, ngx.status) and
-           validators.check_request_method() then
-            local cache_ttl = cache:cache_ttl()
-            if cache_ttl ~= nil then
-                ngx.log(ngx.NOTICE, "[cache-update]["..ngx.ctx.cache_key.."]["..cache_ttl.."] updating cache")
-                storage:set(ngx.ctx.cache_key, {
-                    headers = ngx.ctx.headers,
-                    content = body,
-                    status = ngx.status
-                }, cache_ttl)
-            else
-                ngx.log(ngx.NOTICE, "[cache-update] cache TTL is undefined. So the cache was not updated")
-            end
+        local cache_ttl = cache:cache_ttl()
+        if cache_ttl ~= nil then
+            ngx.log(ngx.NOTICE, "[cache-update]["..ngx.ctx.cache_key.."]["..cache_ttl.."] updating cache")
+            storage:set(ngx.ctx.cache_key, {
+                headers = ngx.ctx.headers,
+                content = body,
+                status = ngx.status
+            }, cache_ttl)
         else
-            ngx.log(ngx.NOTICE, "[cache-update] the cache was not updated because of plugin config")
-            return
+            ngx.log(ngx.NOTICE, "[cache-update] cache TTL is undefined. So the cache was not updated")
         end
     else
         ngx.log(ngx.NOTICE, "[cache-update] getting chunk #"..ngx.ctx.rt_body_chunk_number)
