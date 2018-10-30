2e3abe81d1ed16adba6d8b46dde077274bdb530e
Patrick Porto <patrick.porto@corp.globo.com>
2018-10-30 15:40:04 -0300

    Add filter to remove hop-by-hop headers

--- proxy-cache/body_filter.lua
+++ proxy-cache/body_filter.lua
@@ -5,6 +5,30 @@ local Encoder = require 'kong.plugins.proxy-cache.encoder'
 
 local _M = {}
 
+local function filter_headers(headers)
+    -- remove hop-by-hop headers
+    headers["Connection"] = nil
+    headers["connection"] = nil
+    headers["Keep-Alive"] = nil
+    headers["keep-alive"] = nil
+    headers["Public"] = nil
+    headers["public"] = nil
+    headers["Proxy-Authenticate"] = nil
+    headers["proxy-authenticate"] = nil
+    headers["Transfer-Encoding"] = nil
+    headers["transfer-encoding"] = nil
+    headers["Upgrade"] = nil
+    headers["upgrade"] = nil
+    headers["Via"] = nil
+    headers["via"] = nil
+    -- remove kong custom headers
+    headers["X-Kong-Upstream-Latency"] = nil
+    headers["X-Kong-Proxy-Latency"] = nil
+    -- remove plugin custom headers
+    headers["X-Cache-Status"] = nil
+    return headers
+end
+
 local function async_update_cache(config, cache_key, body)
     local headers = ngx.resp.get_headers(0, true)
     local status = ngx.status
@@ -15,8 +39,7 @@ local function async_update_cache(config, cache_key, body)
         storage:set_config(config)
         local cache_ttl = cache:cache_ttl()
         if cache_ttl ~= nil then
-            headers["Connection"] = nil
-            local cache_value = Encoder.encode(status, body, headers)
+            local cache_value = Encoder.encode(status, body, filter_headers(headers))
             storage:set(cache_key, cache_value, cache_ttl)
         end
     end)
